af61de24639cb7d364554ba2f4d4cae53e7c92c5
rsk5b <randall.s.kelley@gmail.com>
2014-02-06 19:13:58 -0500

    updated Randy's files, added prob distributions

--- support_functions.R
+++ support_functions.R
@@ -1,7 +1,49 @@
 require(compiler)
 enableJIT(3)
 
-source("~/Development/stat183/packages/gol_simulator/gol_simulator.R")
+source("gol_simulator.R")
+require(R.utils)
+require(stringr)
+
+
+## find a submatrix of the 20x20 matrix
+get_submatrix = function(X, row = 1, col = 1,  L = 3)
+{
+  
+  # build interval
+  if( L %% 2 != 0)  # odd case
+  {
+    i_row = max(row-floor(L/2), 1):min(row+floor(L/2), 20)
+    i_col = max(col-floor(L/2), 1):min(col+floor(L/2), 20)
+  }  
+  else # even case  
+  {
+    i_row = max(row-L/2, 1):min(row+L/2-1, 20)
+    i_col = max(col-L/2, 1):min(col+L/2-1, 20)
+  }  
+  
+  # consider row edges
+  X_sub   =  X[i_row, i_col]
+  if( row == 1) 
+  {
+    X_sub = rbind( matrix(0, ncol = ncol(X_sub), nrow = ifelse(L %% 2 != 0, floor(L/2), L/2)), X_sub)
+  }
+  else if( row == 20)
+  {
+    X_sub = rbind(X_sub, matrix(0, ncol = ncol(X_sub), nrow = ifelse(L %% 2 != 0, floor(L/2), L/2-1)) )
+  }
+  
+  # consider column edges
+  if( col == 1 )
+  {
+    X_sub = cbind( matrix(0, nrow = nrow(X_sub), ncol = floor(L/2)), X_sub)
+  }
+  else if( col == 20)
+  {
+    X_sub = cbind(X_sub, matrix(0, nrow = nrow(X_sub), ncol = ifelse(L %% 2 != 0, floor(L/2), L/2-1)) )
+  }
+  return(X_sub)
+}
 
 ## build a distribution for a point in the grid
 build_distribution = function(
@@ -16,18 +58,10 @@ build_distribution = function(
   file_path = "data/"  # path to the file 
 )
 {
-  source("~/Development/stat183/packages/gol_simulator/gol_simulator.R")
-  require(stringr)
-  
-  # check if the point is in the grid
-  if( L %% 2 == 0) stop("L must be odd")
-  
-  # select the intervals of the subgrid to analyze
-  i_row = max(point[1]-floor(L/2), 1):min(point[1]+floor(L/2), n_row)
-  i_col = max(point[2]-floor(L/2), 1):min(point[2]+floor(L/2), n_col)
-  
+
   ## initialize data frame for storing results
-  prob_dist = data.frame(name = character(), is_on = integer(), count = integer())
+  prob_dist = data.frame( character(),  integer(),  integer())
+  names(prob_dist) = c("name", "is_on", "count")
   
   for( i in 1:N_samples)
   {
@@ -39,116 +73,129 @@ build_distribution = function(
     
     ## print out status updates
     if( i %% 1e3 == 0) cat("\n", i , "complete\n")
-    
-    ## select a subblock of cose
+  
+    # determine wether board is on from start point
     is_on      = X[point[1], point[2], 5]
-    X_subset_1 = X[i_row, i_col,5]
-    X_subset_1 = X[i_row, i_col,6]
+    
+    ## select a subset of the ending board
+    X_stop = get_submatrix(X[,,6], row = point[1], col = point[2], L = L)
     
     # classify end board
     # flatten the matrix, by row
-    tag   = str_replace_all( toString(as.integer(X_subset_1)), ", ", "")
-    
+    tag   = str_replace_all( toString(as.integer(X_stop)), ", ", "")
+  
+    # find entry in the distribution the cooresponds to the given submatrix
     index = which(prob_dist$name == tag)
-    
+
+    # if the length of the index array is zero, then add a new entry to the data frame
     if( length(index) == 0)
     {
       prob_dist = rbind(prob_dist,  data.frame(name = tag, is_on = is_on, count = 1 ))
-    } else
+    }
+    # otherwise, increment the counter and update the is_on field
+    else
     {
       prob_dist$is_on[index] = prob_dist$is_on[index]+ is_on
       prob_dist$count[index] = prob_dist$count[index]+ 1
     }
   }
-  
+
+  # compute the probability, that given the configuration, the count is on
   prob = with(prob_dist, is_on/count)
   prob_dist = cbind(prob_dist, prob = prob)
   
+  # the the distribution and the size of the submatrix in a list to be returned
   prob_list = list( dist = prob_dist, L = L)
   
   # save the data frame
   if( save_file)
   {
-    file_name = paste0("prod_dist", file_extension, ".Rda")
+    file_name = paste0("prob_dist", file_ext, ".Rda")
     path_name = paste0(file_path, file_name)
-    #saveRDS(prob_list, file = path_name)
     save(prob_list, file = path_name)
   }
   return(prob_list)
-  
 }
 
-pred_start_board = function(X_end, prob_list, prob_thr = .5,  verbose = FALSE)
+## make a prediction
+# using an error threshold
+pred_start_board = function(X_stop, prob_list, prob_thr = 0.5, error_thr = .1, N_max = 50, verbose = FALSE)
 {
-  L = prob_list$L
-  prob_dist = prob_list$dist
-  if(verbose)
-  { 
-    cat("\n\nL = ", L)
-    print(prob_dist)
-  }
-  
-  
-  X_start = matrix(0, 20,20)
-  
-  # double loop over the board
-  for( row in 1:20)
-  {
-    for( col in 1:20)
-    { 
-      if(verbose){cat("\n\nrow =", row, "col = ", col)}
-      # build interval
-      i_row = max(row-floor(L/2), 1):min(row+floor(L/2), 20)
-      i_col = max(col-floor(L/2), 1):min(col+floor(L/2), 20)
-      
-      if(verbose)
-      {
-        cat("\ni_row = ", i_row)
-        cat("\ni_col = ", i_col)
-      }
-      
-      # consider row edges
-      X_sub   =  X_end[i_row, i_col]
-      if( row == 1) 
-      {
-        X_sub = rbind( matrix(0, ncol = ncol(X_sub), nrow = floor(L/2)), X_sub)
-      }
-      else if( row == 20)
-      {
-        X_sub = rbind(X_sub, matrix(0, ncol = ncol(X_sub), nrow = floor(L/2)) )
-      }
-      
-      # consider column edges
-      if( col == 1 )
-      {
-        X_sub = cbind( matrix(0, nrow = L, ncol = floor(L/2)), X_sub)
-      }
-      else if( col == 20)
-      {
-        X_sub = cbind(X_sub, matrix(0, nrow = L, ncol = floor(L/2)) )
-      }
-      
-      # flatten the matrix, by row
-      tag   = str_replace_all( toString(as.integer(X_sub)), ", ", "")
-      
-      index = with(prob_dist, which(name == tag))
-      if(verbose)
-      {
-        cat("\nindex = ", index)
-        cat("\nlength(index) = ", length(index))
-      }
-      if( length(index) == 0)
-      {
-        if(verbose){cat("\nno match, setting value to zero")}
-        X_start[row,col] = 0
-      } 
-      else
-      {
-        prob  = with(prob_dist, prob[index])
-        X_start[row, col] = ifelse(prob < prob_thr, 0, 1)
-      }
-    } # end col loop
-  } # end row loop
-  
-  return(X_start)
+    # X_stop: the stop board that we are trying to evolve backwards
+    # prob_list: list that contains a data.frame (probability distribution) and the size of the submatrix classifier
+    # prob threshold: use to set the inital guess of the submatrices
+    # error_thr:  the threshold for error we are willing to except
+    # N_max:  if the while loop continues and we have not achieved the desired threshold, kill the loop
+    # verbose:  TRUE means printouts
+    
+    # retrieve the size of the submatrix from the prob_list
+    L = prob_list$L
+    
+    # retrieve the prob_dist from the list
+    prob_dist = prob_list$dist
+    
+    # define forward error, that the error the prediction makes when it's evolved forward one step
+    # and this compared to the known stop board.
+    forward_error_curr = 1  # for each sample, compute the forward error
+    forward_error_best = 1  # keep track of the sample with the best error
+    X_curr  = matrix(0, 20,20 )  # current prediction using a random throw
+    X_best  = matrix(0, 20,20 )  # the best one of the samples 
+    
+    # initalize the counter
+    count = 0;
+    while(forward_error_curr > error_thr && count < N_max)
+    {
+        # keep track of how many iterations
+        count = count + 1;
+        
+        #scan through the board and select sub-matrices
+        for( row in 1:20)
+        {
+            for( col in 1:20)
+            {
+                # get sub matrix
+                X_sub = get_submatrix(X_stop, row = row, col = col, L=L)
+                
+                # flatten the matrix, by row and cast it to string
+                tag   = str_replace_all( toString(as.integer(X_sub)), ", ", "")
+                
+                # find the index where the tag occures in the probability data-frame
+                index = with(prob_dist, which(name == tag))
+
+                # if the sub-matrix does not have an entry in the prob-data frame, 
+                # assume the cell is zero (possible mistake here)
+                if( length(index) == 0)
+                {
+                    X_curr[row,col] = 0
+                    if(count == 1) X_best[row, col] = 0
+                } 
+                else
+                {
+                    # sample the boards using the emperical pdf 
+                  prob  = with(prob_dist, prob[index])
+                  X_curr[row, col] = rbinom(1, prob = prob, size = 1)
+
+                  # for the first iteration, assume the best is the one given by
+                  # using prob > prob_thr (=0.5) to decide
+                  if(count == 1) X_best[row, col] = ifelse(prob < prob_thr, 0, 1)
+                }
+            } # end col loop
+        }# end row loop
+        
+        # evolve prediction and compare with the start board.
+        if(count == 1) forward_error_best = sum(as.integer( update_board(X_best) != X_stop))/400
+        forward_error_curr = sum(as.integer( update_board(X_curr) != X_stop))/400
+        
+        if( forward_error_curr < forward_error_best )
+        {
+            X_best = X_curr
+            forward_error_best = forward_error_curr
+        } 
+    } # end while loop
+    
+    if(verbose){ cat( '\n\tForward error = ',  forward_error_best, "\t N_throws = ",  count)}
+    return(X_best)
 }
+
+
+#
